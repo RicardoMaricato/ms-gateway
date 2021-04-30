80c142f3d57ffe874d3cb8bba378fc7eb36ac2df
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-04-30 00:43:29 -0300

    -Project created

---
+++ src/test/java/br/com/ricardomaricato/msgateway/MsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ricardomaricato.msgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
